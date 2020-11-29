3c4d6299ab8ec6c59255ed95dde81092c183656f
Lena Rodziewicz <lena@rodziewicz.com.pl>
2020-11-30 00:20:34 +0100

    Added consumer and producer. I think it works correctly but there might be unnessesary locking.

--- MultithreadingBufferExcersise/Buffer.hpp
+++ MultithreadingBufferExcersise/Buffer.hpp
@@ -2,11 +2,14 @@
 #include <deque>
 #include <vector>
 
+// I have spent so much more time then it's been needed just by using templates
+// for this implementation
 template <typename T>
 class Buffer
 {
 private:
 	std::deque<T> container;
+	std::mutex mu;
 	unsigned int maxSize;
 	unsigned int batchSize;
 
@@ -18,15 +21,23 @@ public:
 	Buffer<T>(int unsigned maxSize, int unsigned batchSize = 10) : maxSize(maxSize), batchSize(batchSize)
 	{ }
 
+	~Buffer() {
+		// Everything should be cleared automatically... I think...
+	}
+
 	unsigned int getCurrentSize() {
 		return container.size();
 	}
 
+	// This implementation assumes that by "lewa strona" you meant front and vice versa
 	void pushValue(T value) {
+		// I guess since C++17 we should use std::scoped_lock instead
+		std::lock_guard<std::mutex> lock(mu);
 		if (container.size() == maxSize) {
 			container.pop_back();
 		}
 		container.push_front(value);
+		// Unlock is called once lock falls out of scope
 	}
 
 	std::vector<T>* flush() {
@@ -35,6 +46,7 @@ public:
 		// If the buffer is empty returns empty
 		if (container.empty()) return batch;
 
+		std::lock_guard<std::mutex> lock(mu);
 		unsigned int currentBatchSize = getCurrentBatchSize();
 		typename std::deque<T>::iterator it;
 		for (it = container.begin(); it != (container.begin() + currentBatchSize); it++) {
--- MultithreadingBufferExcersise/MultithreadingBufferExcersise.cpp
+++ MultithreadingBufferExcersise/MultithreadingBufferExcersise.cpp
@@ -3,51 +3,87 @@
 
 #include <iostream>
 #include <conio.h>
-
 #include <deque>
 #include <vector>
 #include <mutex>
-#include "Buffer.h"
+#include <random>
 
+#include "Buffer.hpp"
+
+static std::random_device rd;
+static std::mt19937 gen(rd());
 
 std::mutex mu;
 std::condition_variable condition;
 std::deque<float> buffer;
-const unsigned int bufferSize = 100;
-
-void producer() {
-
-}
-
-void consumer() {
-
-}
+bool shouldRun = true;
+const unsigned int BUFFER_SIZE = 20;
+const unsigned int BATCH_SIZE = 10;
 
 //template <typename T>
-void printVector(std::vector<double> *values) {
+void printVector(std::vector<double>* values) {
     // Figuring it out took so loong... 
     std::cout << "Current buffer batch:\n";
-    for (auto &&value : *values) {
+    for (auto&& value : *values) {
         std::cout << value << ",\t";
     }
     std::cout << "\n";
 }
 
+double getRandomNumber() {
+    std::uniform_real_distribution<> dis(0, 100);
+    return std::sin(dis(gen));
+}
+
+void producer(std::shared_ptr<Buffer<double>> buffer) {
+    while (shouldRun) {
+
+        std::printf("Producer: Size of the buffer %u\n", buffer->getCurrentSize());
+        // I wasn't sure if sin(x) form the description meant some sort of rnumb
+        // or sin(times_of_execution). Random number avoids overflowing loop counter
+        double rvalue = getRandomNumber();
+        std::printf("Producer: Random signal %f\n", rvalue);
+
+        buffer->pushValue(rvalue);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+void consumer(std::shared_ptr<Buffer<double>> buffer) {
+    while (shouldRun) {
+        std::printf("Consumer: Size of the whole buffer: %u\n", buffer->getCurrentSize());
+
+        std::vector<double>* batch = buffer->flush();
+        std::printf("Consumer: Size of the batch: %u\n", batch->size());
+        printVector(batch);
+        delete batch;
+
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+}
+
 int main()
 {
-    Buffer<double> *buffer = new Buffer<double>(2, 10);
+    // Requirements say that implementation should allow for only one instance
+    // Easiest impementation is to use singletion but it has multiple drawbacks
+    // Anternatively we could use service locator or DI (will be added if I have enough time)
+    std::shared_ptr<Buffer<double>> buffer(new Buffer<double>(BUFFER_SIZE, BATCH_SIZE));
 
-    std::printf("Size of buffer: %u\n", buffer->getCurrentSize());
-    buffer->pushValue(1.2);
-    buffer->pushValue(3.4);
-    buffer->pushValue(4.1);
-    std::printf("Size of buffer: %u\n", buffer->getCurrentSize());
-    
-    std::vector<double>* batch = buffer->flush();
-    printVector(batch);
+    std::cout << "Press any key to stop threads...\n";
 
-    delete batch;
-    delete buffer;
+
+    std::thread t1(producer, buffer);
+    std::thread t2(consumer, buffer);
+
+    _getch();
+    shouldRun = false;
+
+    t1.join();
+    t2.join();
+
+    std::printf("Size of the buffer: %u\n", buffer->getCurrentSize());
+
 
     return 0;
 }
